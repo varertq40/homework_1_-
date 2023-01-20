60cd91badea9996a0551fb93c71b189ecfe180fb
varertq40 <110782641+varertq40@users.noreply.github.com>
2023-01-20 18:23:25 +0300

    Update Program.cs

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -5,11 +5,11 @@
 // 22 3 9 -> 22
 
 Console.Write("Введите первое число ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число ");
-int b = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine()!);
 Console.Write("Введите третье число ");
-int c = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine()!);
 
 int max = 0;
 
